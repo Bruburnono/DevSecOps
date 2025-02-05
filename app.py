e238ba6b18a0977201add8a5911a50039c45479e
Arifea69 <tfazendateles@guardiaschool.fr>
2025-02-05 12:21:18 +0100

    app.py

--- app.py
+++ app.py
@@ -2,35 +2,20 @@ import flask
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask import request, jsonify
 from flask_mysqldb import MySQL
+from flask_wtf import CSRFProtect
 import bcrypt
 
 app = flask.Flask(__name__)
 app.config["DEBUG"]=True
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'base'
 mysql = MySQL(app)
 
 app.secret_key= b'\xe6\xe3\\\x0bp\xe6\x9f\xfcT\xfa\xa1\r<\xab\xf9\x1f\x87\xad\xb8\xf0\x17\x9f\x9c\xbd'
-
-books = [
-    {'id': 0,
-    'title': 'A Fire Upon the Deep',
-    'author': 'Vernor Vinge',
-    'first_sentence': 'The coldsleep itself was dreamless.',
-    'year_published': '1992'},
-    {'id': 1,
-    'title': 'The Ones Who Walk Away From Omelas',
-    'author': 'Ursula K. Le Guin',
-    'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-    'year_published': '1973'},
-    {'id': 2,
-    'title': 'Dhalgren',
-    'author': 'Samuel R. Delany',
-    'first_sentence': 'to wound the autumnal city.',
-    'year_published': '1975'}
-]
+csrf = CSRFProtect(app)
+app.config['WTF_CSRF_ENABLED'] = True
 
 
 def hash_password(password):
@@ -44,9 +29,6 @@ def verify_password(stored_hash, entered_password):
 def home():
     return render_template("login.html")
 
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    return render_template("booksall.html", books=books)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -156,9 +138,6 @@ def api_id():
     else:
         return "Error: No id field provided. Please specify an id."
     results = []
-    for book in books:
-        if book['id'] == ID:
-            results.append(book)
-    return render_template("booksall.html", books=results)
+
 
 app.run()
